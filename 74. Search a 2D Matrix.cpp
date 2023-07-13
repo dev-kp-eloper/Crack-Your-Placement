12bfc94327c3430d471614095a23319e8f993cda
dev-kp-eloper <99352163+dev-kp-eloper@users.noreply.github.com>
2023-07-13 12:57:01 +0530

    Create 74. Search a 2D Matrix.cpp

---
+++ 74. Search a 2D Matrix.cpp
@@ -0,0 +1,24 @@
+//Link - https://leetcode.com/problems/search-a-2d-matrix/description/
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int start = 0;
+        int end = (m*n)-1;
+        while(start <= end){
+            int mid = (start + end)/2;
+                if(matrix[mid/n][mid%n] == target){
+                    return true;
+                }
+                else if(matrix[mid/n][mid%n] < target){
+                    start = mid + 1;
+                }
+                else{
+                    end = mid - 1;
+                }
+        }
+        return false;
+    }
+};
