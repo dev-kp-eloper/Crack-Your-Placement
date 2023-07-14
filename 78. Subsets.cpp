43330d7667d65bc3aa88a66064813a556ab37296
dev-kp-eloper <99352163+dev-kp-eloper@users.noreply.github.com>
2023-07-15 00:22:33 +0530

    Create 78. Subsets.cpp

---
+++ 78. Subsets.cpp
@@ -0,0 +1,21 @@
+//Link - https://leetcode.com/problems/subsets/description/
+
+class Solution {
+public:
+    void findsubset(vector<int>& nums, vector<vector<int>>& ans, vector<int>& ds, int idx){
+        ans.push_back(ds);
+        for(int i=idx; i<nums.size(); i++){
+            ds.push_back(nums[i]);
+            findsubset(nums,ans,ds,i+1);
+            ds.pop_back();
+        }
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+        vector<int> ds;
+        findsubset(nums,ans,ds,0);
+        return ans;
+    }
+};
