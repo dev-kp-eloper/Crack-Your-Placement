4705898326db057a8f7ba9367d9fa4a468437179
dev-kp-eloper <99352163+dev-kp-eloper@users.noreply.github.com>
2023-07-30 20:11:57 +0530

    Create 198. House Robber.cpp

---
+++ 198. House Robber.cpp
@@ -0,0 +1,22 @@
+//Link - https://leetcode.com/problems/house-robber/description/
+
+class Solution {
+public:
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        int prev = nums[0];
+        int prev2 = 0;
+        for(int i=1; i<n; i++){
+            int pick = nums[i];
+            if(i>1){
+                pick += prev2;
+            }
+            int notpick = 0 + prev;
+            int curri = max(pick, notpick);
+            prev2 = prev;
+            prev = curri; 
+        }
+        return prev;
+    }
+};
