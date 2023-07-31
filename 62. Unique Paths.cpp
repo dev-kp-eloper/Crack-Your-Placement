1b649a1e540062ea468b3105a8995433fda64174
dev-kp-eloper <99352163+dev-kp-eloper@users.noreply.github.com>
2023-07-31 14:50:07 +0530

    Create 62. Unique Paths.cpp

---
+++ 62. Unique Paths.cpp
@@ -0,0 +1,60 @@
+//Link - https://leetcode.com/problems/unique-paths/description/
+
+class Solution {
+public:
+    //MEMOIZATION
+    // int f(int i, int j, vector<vector<int>> &dp){
+    //     if(i == 0 and j == 0) return 1;
+    //     if(i < 0 or j < 0) return 0;
+        
+    //     if(dp[i][j] != -1) return dp[i][j];
+
+    //     int up = f(i-1, j, dp);
+    //     int left = f(i, j-1, dp);
+
+    //     return dp[i][j] = up + left;
+    // }
+
+    //TABULATION
+    // int uniquePaths(int m, int n) {
+    //     vector<vector<int>> dp(m, vector<int>(n, 0));
+    //     dp[0][0] = 1;
+
+    //     for(int i = 0; i < m; i++){
+    //         for(int j = 0; j < n; j++){
+    //             if(i == 0 and j == 0) dp[0][0] = 1;
+    //             else{
+    //                 int up = 0, left = 0;
+    //                 if(i > 0) up = dp[i-1][j];
+    //                 if(j > 0) left = dp[i][j-1];
+
+    //                 dp[i][j] = up + left;
+    //             }
+    //         }
+    //     }
+
+    //     return dp[m-1][n-1];
+    // }
+
+    //SPACE OPTIMISATION TO 1D DP
+    int uniquePaths(int m, int n) {
+        vector<int> prev(n, 0);
+
+        for(int i = 0; i < m; i++){
+            vector<int> curr(n, 0);
+            for(int j = 0; j < n; j++){
+                if(i == 0 and j == 0) curr[j] = 1;
+                else{
+                    int up = 0, left = 0;
+                    if(i > 0) up = prev[j];
+                    if(j > 0) left = curr[j-1];
+
+                    curr[j] = up + left;
+                }
+            }
+            prev = curr;
+        }
+
+        return prev[n-1];
+    }
+};
