9cc5081c14005ce14fbb5090a12771f73bbe6d11
dev-kp-eloper <99352163+dev-kp-eloper@users.noreply.github.com>
2023-08-18 00:31:10 +0530

    Create 56. Merge Intervals.cpp

---
+++ 56. Merge Intervals.cpp
@@ -0,0 +1,22 @@
+//Link - https://leetcode.com/problems/merge-intervals/description/
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        vector<vector<int>> ans;
+        sort(intervals.begin(), intervals.end());
+        ans.push_back(intervals[0]);
+        int j = 0;
+        for(int i=1; i<n; i++){
+            if(ans[j][1] >= intervals[i][0]){
+                ans[j][1] = max(ans[j][1], intervals[i][1]);
+            }
+            else{
+                j++;
+                ans.push_back(intervals[i]);
+            }
+        }
+        return ans;
+    }
+};
