efa1d7d5b3d4a09e6873e0866b185fd85eaf3319
dev-kp-eloper <99352163+dev-kp-eloper@users.noreply.github.com>
2023-09-02 00:32:17 +0530

    Create 100. Same Tree.cpp

---
+++ 100. Same Tree.cpp
@@ -0,0 +1,31 @@
+//Link - https://leetcode.com/problems/same-tree/description/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == NULL or q == NULL){
+            return (p == q);
+        }
+
+        if(p -> val != q -> val){
+            return false;
+        }
+
+        bool P_ans = isSameTree(p -> left, q -> left);
+        bool Q_ans = isSameTree(p -> right, q -> right);
+
+        return (P_ans and Q_ans);
+
+    }
+};
