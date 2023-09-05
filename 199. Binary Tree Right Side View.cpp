ddc1c461b48245b3d758b8c002c4cd4f9740bf01
dev-kp-eloper <99352163+dev-kp-eloper@users.noreply.github.com>
2023-09-06 01:01:04 +0530

    Create 199. Binary Tree Right Side View.cpp

---
+++ 199. Binary Tree Right Side View.cpp
@@ -0,0 +1,30 @@
+//Link - https://leetcode.com/problems/binary-tree-right-side-view/description/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void solve(TreeNode* root, vector<int>& ans, int level){
+        if(root == NULL) return;
+
+        if(level == ans.size()) ans.push_back(root -> val);
+
+        if(root -> right) solve(root -> right, ans, level+1);
+        if(root -> left) solve(root -> left, ans, level+1);
+    }
+
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans;
+        solve(root, ans, 0);
+        return ans;
+    }
+};
